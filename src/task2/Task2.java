b61aae0ad2813339d1d356c9827cbf9dd526a429
Kaplupepe <kaplupepe@gmail.com>
2021-09-03 03:43:40 +0300

    scanner optimization

--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -6,7 +6,6 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Scanner sc1 = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
         Double firstNum;
         Double secondNum;
         Double result = 0.0;
@@ -15,7 +14,7 @@ public class Task2 {
         System.out.println("Choose the operation!");
         String operator = sc1.nextLine();
         System.out.println("The second number to enter!");
-        secondNum = sc2.nextDouble();
+        secondNum = sc.nextDouble();
         if (operator.equals("+")) {
             result = firstNum + secondNum;
         } else if (operator.equals("-")) {
